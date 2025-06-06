f5c5d06fb05a2f65a7f8171dd00344354a18b303
Hridoy032003 <hridoy03102003@gmail.com>
2025-06-06 14:18:15 +0530

    hh

--- index.js
+++ index.js
@@ -21,10 +21,48 @@ setInterval(() => {
 // menu
 
 
-let iconMenu = document.querySelector(".sider-div > .icon");     
+let iconMenu = document.querySelector(".sider-div ");     
 let body = document.querySelector("body");
-
+let closeMenu = document.getElementById("close");
 iconMenu.addEventListener("click", () => {
-    body.classList.toggle("showCart"); // Toggles the class on body
-    console.log("clicked");
+    body.classList.toggle("showCart"); 
+    
 });
+closeMenu.addEventListener("click", () => {
+    body.classList.toggle("showCart");
+   
+});
+function close() {
+    body.classList.remove("showCart");
+}
+const addDataToHTML = (listFood)=>{
+   
+    
+       listFood.forEach(element => {
+        let newElement = document.createElement("div");
+           newElement.classList.add("food-card");
+        newElement.innerHTML = `
+            <img src="${element.image}" alt="${element.name}">
+            <h3>${element.name}</h3>
+            <p>${element.description}</p>
+            <span>$${element.price}</span>
+             <button class="add-to-cart">Add to Cart</button>
+        `;
+           listFood.appendChild(newElement);
+        
+       });
+    
+}
+addDataToHTML();
+// DATA OF FOOD
+const initApp = () => {
+    fetch("./food.json")
+        .then(response => response.json())  
+        .then(data => {
+            listFood=data;
+            console.log(data);
+            addDataToHTML();
+        })}
+initApp(listFood);
+
+ 
